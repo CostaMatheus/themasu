9720103f53d7be6ac1db5a743fedac135aca9e75
CostaMatheus <fernandesdacostamatheus@gmail.com>
2024-12-21 13:11:38 -0300

    menu toggle animation

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let menuHamburger = document.querySelector(".menu-hamburger");
+let linksContainer = document.querySelector(".links-container");
+
+menuHamburger.addEventListener("click", function () {
+    linksContainer.classList.toggle("active");
+    menuHamburger.classList.toggle("active");
+});
+
+window.onscroll = () => {
+    linksContainer.classList.remove("active");
+    menuHamburger.classList.toggle("active");
+};
